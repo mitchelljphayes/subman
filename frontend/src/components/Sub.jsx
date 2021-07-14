9598fe9f87db9df39eb6519f20d3198f6014465d
Harpo Barrett <hbar0009@student.monash.edu>
2021-07-15 00:34:40 +1000

    Added animation to nav bar, added react-transition-group to dependencies

--- frontend/src/components/Sub.jsx
+++ frontend/src/components/Sub.jsx
@@ -2,23 +2,21 @@ import React from 'react'
 
 const Sub = (props) => {
     return (
-       // <div className="sub-group flex space-between">
-            <div className="sub-item flex space-between">
-                <div className="sub-icon-background flex">
-                    {props.subIcon}
-                </div>
-                <div className="sub-description flex">
-                    <div className="textarea">
-                        <p className="f3">{props.subName}</p>
-                        <span className="f5 bold">{props.subPrice}</span><span className="f2 gray"> /per {props.subTimePeriod}</span>
-                    </div>
-                    
-                </div>
-                <div className="sub-time-left-icon-background flex">
-                    {props.subTimeLeftIcon}
-                </div>
+        <div className="sub-item flex space-between">
+            <div className="sub-icon-background flex">
+                {props.subIcon}
             </div>
-        //</div>
+            <div className="sub-description flex">
+                <div className="textarea">
+                    <p className="f3">{props.subName}</p>
+                    <span className="f5 bold">{props.subPrice}</span><span className="f2 gray"> /per {props.subTimePeriod}</span>
+                </div>
+                
+            </div>
+            <div className="sub-time-left-icon-background flex">
+                {props.subTimeLeftIcon}
+            </div>
+        </div>
     )
 }
 
