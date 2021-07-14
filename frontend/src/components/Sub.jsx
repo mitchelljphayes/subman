f2ec19fa2a0bf780bf517019de5ff8b726721924
Harpo Barrett <hbar0009@student.monash.edu>
2021-07-14 16:49:45 +1000

    Auto stash before merge of "main" and "origin/main"

--- frontend/src/components/Sub.jsx
+++ frontend/src/components/Sub.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 
-const Sub = () => {
+const Sub = (props) => {
     return (
-        <div>
-            
-        </div>
+       // <div className="sub-group flex space-between">
+            <div className="sub-item flex space-between">
+                <div className="sub-icon-background flex">
+                    {props.subIcon}
+                </div>
+                <div className="sub-description flex">
+                    <div className="textarea">
+                        {/* <h4>{props.subName}</h4> */}
+                        {/* <p className="sub-price">{props.subPrice}</p>
+                        <p className="sub-time-period">{props.subTimePeriod}</p> */}
+                        <p className="f3">{props.subName}</p>
+                        <span className="f5 bold">{props.subPrice}</span><span className="f2 gray"> /per {props.subTimePeriod}</span>
+                    </div>
+                    
+                </div>
+                <span className="sub-time-left-icon">{props.subTimeLeftIcon}</span>
+            </div>
+        //</div>
     )
 }
 
--- frontend/src/views/HomePage.jsx
+++ frontend/src/views/HomePage.jsx
@@ -1,11 +1,39 @@
 import React from 'react'
 import Nav from '../components/Nav'
+import Sub from "../components/Sub"
+
+/* TEMPORARY, FOR TESTING PURPOSES */
+import {ReactComponent as YoutubeLogo} from '../icons/youtube-logo.svg'
 
 const HomePage = () => {
     return (
         <div className="container">
             Home Page
             <Nav/>
+            <Sub 
+                subIcon={<YoutubeLogo className="sub-icon"/>}
+                subName="Youtube Premium"
+                subPrice="$99.99"
+                subTimePeriod="month"
+            />
+            <Sub 
+                subIcon={<YoutubeLogo className="sub-icon"/>}
+                subName="Youtube Premium"
+                subPrice="$99.99"
+                subTimePeriod="month"
+            />
+            <Sub 
+                subIcon={<YoutubeLogo className="sub-icon"/>}
+                subName="Youtube Premium"
+                subPrice="$99.99"
+                subTimePeriod="month"
+            />
+            <Sub 
+                subIcon={<YoutubeLogo className="sub-icon"/>}
+                subName="Youtube Premium"
+                subPrice="$99.99"
+                subTimePeriod="month"
+            />
         </div>
     )
 }
