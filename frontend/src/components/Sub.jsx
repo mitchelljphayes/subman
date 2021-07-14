f167148d113b27f76d96034f62ca57977f9c9eeb
Harpo Barrett <hbar0009@student.monash.edu>
2021-07-14 17:13:00 +1000

    Actually added files, git was acting up

--- frontend/src/components/Sub.jsx
+++ frontend/src/components/Sub.jsx
@@ -9,15 +9,14 @@ const Sub = (props) => {
                 </div>
                 <div className="sub-description flex">
                     <div className="textarea">
-                        {/* <h4>{props.subName}</h4> */}
-                        {/* <p className="sub-price">{props.subPrice}</p>
-                        <p className="sub-time-period">{props.subTimePeriod}</p> */}
                         <p className="f3">{props.subName}</p>
                         <span className="f5 bold">{props.subPrice}</span><span className="f2 gray"> /per {props.subTimePeriod}</span>
                     </div>
                     
                 </div>
-                <span className="sub-time-left-icon">{props.subTimeLeftIcon}</span>
+                <div className="sub-time-left-icon-background flex">
+                    {props.subTimeLeftIcon}
+                </div>
             </div>
         //</div>
     )
--- frontend/src/views/HomePage.jsx
+++ frontend/src/views/HomePage.jsx
@@ -5,6 +5,8 @@ import Sub from "../components/Sub"
 
 /* TEMPORARY, FOR TESTING PURPOSES */
 import {ReactComponent as YoutubeLogo} from '../icons/youtube-logo.svg'
+import {ReactComponent as PieChart} from '../icons/pie-chart.svg'
+
 
 const HomePage = () => {
     return (
@@ -17,24 +19,28 @@ const HomePage = () => {
                 subName="Youtube Premium"
                 subPrice="$99.99"
                 subTimePeriod="month"
+                subTimeLeftIcon={<PieChart className="sub-time-left-icon"/>}
             />
             <Sub 
                 subIcon={<YoutubeLogo className="sub-icon"/>}
-                subName="Youtube Premium"
-                subPrice="$99.99"
-                subTimePeriod="month"
+                subName="Netflix"
+                subPrice="$32.50"
+                subTimePeriod="week"
+                subTimeLeftIcon={<PieChart className="sub-time-left-icon"/>}
             />
             <Sub 
                 subIcon={<YoutubeLogo className="sub-icon"/>}
-                subName="Youtube Premium"
-                subPrice="$99.99"
-                subTimePeriod="month"
+                subName="Figma"
+                subPrice="$100"
+                subTimePeriod="year"
+                subTimeLeftIcon={<PieChart className="sub-time-left-icon"/>}
             />
             <Sub 
                 subIcon={<YoutubeLogo className="sub-icon"/>}
-                subName="Youtube Premium"
-                subPrice="$99.99"
-                subTimePeriod="month"
+                subName="Monash University"
+                subPrice="$10000"
+                subTimePeriod="year"
+                subTimeLeftIcon={<PieChart className="sub-time-left-icon"/>}
             />
         </div>
     )
