03b3e08f3c1e5ea822d2e7494725b7d7a1637a7c
Mitch Hayes <mitchelljphayes@gmail.com>
2021-07-14 16:29:57 +1000

    added user api

--- subscription/views.py
+++ subscription/views.py
@@ -1,8 +1,9 @@
 # from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
-from .serializers import SubscriptionSerializer
+from rest_framework import permissions
+from .serializers import UserSerializer, GroupSerializer, SubscriptionSerializer
 from .models import Subscription 
 
 # Create your views here.
@@ -14,3 +15,21 @@ def say_hello(request):
 class SubscriptionView(viewsets.ModelViewSet):
     serializer_class = SubscriptionSerializer
     queryset = Subscription.objects.all()
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
