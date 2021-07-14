dc39c962f7f963729bf828e572d5d365568e0827
Harpo Barrett <hbar0009@student.monash.edu>
2021-07-14 16:49:45 +1000

    Auto stash before merge of "main" and "origin/main"

--- frontend/src/views/HomePage.jsx
+++ frontend/src/views/HomePage.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import Nav from '../components/Nav'
 import TotalSubs from '../components/TotalSubs'
+import Sub from "../components/Sub"
+
+/* TEMPORARY, FOR TESTING PURPOSES */
+import {ReactComponent as YoutubeLogo} from '../icons/youtube-logo.svg'
 
 const HomePage = () => {
     return (
@@ -8,6 +12,30 @@ const HomePage = () => {
             Home Page
             <Nav/>
             <TotalSubs />
+            <Sub 
+                subIcon={<YoutubeLogo className="sub-icon"/>}
+                subName="Youtube Premium"
+                subPrice="$99.99"
+                subTimePeriod="month"
+            />
+            <Sub 
+                subIcon={<YoutubeLogo className="sub-icon"/>}
+                subName="Youtube Premium"
+                subPrice="$99.99"
+                subTimePeriod="month"
+            />
+            <Sub 
+                subIcon={<YoutubeLogo className="sub-icon"/>}
+                subName="Youtube Premium"
+                subPrice="$99.99"
+                subTimePeriod="month"
+            />
+            <Sub 
+                subIcon={<YoutubeLogo className="sub-icon"/>}
+                subName="Youtube Premium"
+                subPrice="$99.99"
+                subTimePeriod="month"
+            />
         </div>
     )
 }
